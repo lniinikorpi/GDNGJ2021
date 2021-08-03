3ca6c4d55a213e093dae8db4cbf87f92c3c3dd81
unknown <thesilentcoder@outlook.com>
2021-08-03 14:05:47 +0300

    Merge master

--- Assets/_scripts/Player.cs
+++ Assets/_scripts/Player.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Transform mainObject;
+
+    Vector2 movement = new Vector2();
+    int movementDelta = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +19,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (movement != new Vector2())
+        {
+            mainObject.position += new Vector3(movement.x / movementDelta, movement.y / movementDelta, 0);
+        }
     }
 
-    public void OnClick()
+    public void OnMove(InputValue value)
     {
-        print("moi");
+        movement = value.Get<Vector2>();
+    }
+
+    public void OnMouseMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+        Vector3 pos = mainCamera.WorldToScreenPoint(transform.position);
+        Vector3 dir = new Vector3(v.x, v.y, 0) - new Vector3(pos.x, pos.y, 0);
+        Vector3 newDir = Vector3.RotateTowards(Vector3.forward, dir, 1, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
     }
 }
